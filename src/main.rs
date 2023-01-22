ad2ff97f1c82c886a9f3ddc32ba18f901901b6be
Arca <arca.artem@gmail.com>
2023-01-22 13:43:42 +0000

    Add logging and some test coverage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use anyhow::Result;
 use clap::Parser;
-use std::fs;
+use log::debug;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use urlencoding;
@@ -10,7 +12,10 @@ struct Cli {
     #[arg(short, long, default_value = "false")]
     decode: bool,
 
-    #[arg(short, long, default_value = "-")]
+    #[arg(short = 's', long = "string", default_value = "", group = "input")]
+    input_string: String,
+
+    #[arg(short, long, default_value = "-", group = "input")]
     input_file: String,
 
     #[arg(short, long, default_value = "-")]
@@ -18,9 +23,38 @@ struct Cli {
 }
 
 fn main() -> io::Result<()> {
+    env_logger::init();
+
     let cli = Cli::parse();
 
-    let reader = create_reader(&cli.input_file);
+    debug!(
+        "input arguments: decode='{}'  input_string='{}'  input_file='{}'  output_file='{}'",
+        cli.decode, cli.input_string, cli.input_file, cli.output_file
+    );
+
+    if cli.input_string.is_empty() {
+        debug!("processing multi-line input");
+        encode_decode_multiline(cli);
+    } else {
+        debug!("processing single-line input");
+        encode_decode_singleline(cli);
+    }
+
+    Ok(())
+}
+
+fn encode_decode_singleline(cli: Cli) {
+    debug!("encode/decode single-line input");
+    let mut writer = create_writer(&cli.output_file);
+
+    let output = encode_decode(&cli, &cli.input_string);
+
+    writer.write_fmt(format_args!("{}", output)).unwrap();
+}
+
+fn encode_decode_multiline(cli: Cli) {
+    debug!("encode/decode multi-line input");
+    let reader = create_reader(&cli.input_file).expect("Cannot create reader");
 
     let mut writer = create_writer(&cli.output_file);
 
@@ -33,23 +67,23 @@ fn main() -> io::Result<()> {
             break;
         }
 
-        let output = encode_decode(&cli.decode, last_input);
+        let output = encode_decode(&cli, &last_input);
 
         writer.write_fmt(format_args!("{}\n", output)).unwrap();
     }
-
-    Ok(())
 }
 
-fn encode_decode(decode: &bool, last_input: String) -> String {
-    let result: String = match decode {
-        true => urlencoding::decode(&last_input).expect("UTF-8").to_string(),
-        false => urlencoding::encode(&last_input).to_string(),
+fn encode_decode(cli: &Cli, input: &String) -> String {
+    debug!("encode/decode input");
+    let result: String = match cli.decode {
+        true => urlencoding::decode(&input).expect("UTF-8").to_string(),
+        false => urlencoding::encode(&input).to_string(),
     };
     result
 }
 
 fn create_writer(output_file: &String) -> Box<dyn Write> {
+    debug!("creating writer for output file '{}'", output_file);
     let writer: Box<dyn Write>;
 
     if output_file == "-" {
@@ -67,13 +101,42 @@ fn create_writer(output_file: &String) -> Box<dyn Write> {
     writer
 }
 
-fn create_reader(input_file: &String) -> Box<dyn BufRead> {
+fn create_reader(input_file: &String) -> Result<Box<dyn BufRead>> {
+    debug!("creating reader for input file '{}'", input_file);
     let reader: Box<dyn BufRead>;
 
     if input_file == "-" {
         reader = Box::new(BufReader::new(io::stdin().lock()));
     } else {
-        reader = Box::new(BufReader::new(fs::File::open(input_file).unwrap()));
+        let file =
+            File::open(input_file).expect(format!("could not read file `{}`", input_file).as_str());
+        reader = Box::new(BufReader::new(file));
     };
-    reader
+    Ok(reader)
+}
+
+#[test]
+fn test_encode_input() {
+    let cli_args = Cli {
+        decode: false,
+        input_file: String::from("-"),
+        output_file: String::from("-"),
+        input_string: String::from(""),
+    };
+
+    let result = encode_decode(&cli_args, &String::from("Hello, world!"));
+    assert_eq!(result, "Hello%2C%20world%21");
+}
+
+#[test]
+fn test_decode_input() {
+    let cli_args = Cli {
+        decode: true,
+        input_file: String::from("-"),
+        output_file: String::from("-"),
+        input_string: String::from(""),
+    };
+
+    let result = encode_decode(&cli_args, &String::from("Hello%2C%20world%21"));
+    assert_eq!(result, "Hello, world!");
 }
